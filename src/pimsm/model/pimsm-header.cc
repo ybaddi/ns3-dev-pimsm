493376f649de1cee08d6ea18ee47311b2252f157
Youssef Baddi <baddiyoussef85@gmail.com>
2017-07-05 20:16:55 +0100

    eddit header librery

--- src/pimsm/model/pimsm-header.cc
+++ src/pimsm/model/pimsm-header.cc
@@ -1,231 +1,159 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ */
 #include "pimsm-header.h"
-
+#include "ns3/address-utils.h"
+#include "ns3/packet.h"
 #include "ns3/log.h"
-
-#define IPV4_ADDRESS_SIZE 4
-#define OLSR_MSG_HEADER_SIZE 12
-#define OLSR_PKT_HEADER_SIZE 4
 
 namespace ns3 {
 
-NS_LOG_COMPONENT_DEFINE ("PIMSMHeader");
+    NS_LOG_COMPONENT_DEFINE ("PIMSMHeader");
 
-namespace pimsm {
+    namespace pimsm {
 
+        PIMSMHeader::PIMSMHeader(MessageType t) :
+                m_type(t), m_valid(true) {
+        }
 
+        TypeId PIMSMHeader::GetTypeId(void) {
+            NS_LOG_INFO("creating the nodes");
+            static TypeId tid = TypeId("ns3::pimsm::PIMSMHeader")
+                    .SetParent<Header>()
+                    .AddConstructor<PIMSMHeader>();
+            return tid;
+        }
 
+        TypeId
+        PIMSMHeader::GetInstanceTypeId(void) const {
+            return GetTypeId();
+        }
 
-TypeId PacketHeader::GetTypeId(void) {
-	NS_LOG_INFO("creating the nodes");
-	static TypeId tid =
-			TypeId("ns3::pimsm::PacketHeader")
-			.SetParent<Header>()
-			.SetGroupName ("Pimsm")
-			.AddConstructor<PacketHeader>();
-	return tid;
-}
-TypeId PacketHeader::GetInstanceTypeId(void) const {
-	return GetTypeId();
-}
+        uint32_t
+        PIMSMHeader::GetSerializedSize(void) const {
+            return 6;
+        }
 
-uint32_t PacketHeader::GetSerializedSize(void) const {
-	return 6;
-}
-void PacketHeader::Serialize(Buffer::Iterator start) const {
-	// The 2 byte-constant
-	start.WriteU8(0xfe);
-	start.WriteU8(0xef);
-	// The data.
-	start.WriteHtonU32(m_data);
-}
-uint32_t PacketHeader::Deserialize(Buffer::Iterator start) {
-	uint8_t tmp;
-	tmp = start.ReadU8();
-	NS_ASSERT(tmp == 0xfe);
-	tmp = start.ReadU8();
-	NS_ASSERT(tmp == 0xef);
-	m_data = start.ReadNtohU32();
-	return 6; // the number of bytes consumed.
-}
+        void
+        PIMSMHeader::Serialize(Buffer::Iterator start) const {
+            // The 2 byte-constant
+            start.WriteU8(0xfe);
+            start.WriteU8(0xef);
+            // the message type
+            start.WriteU8((uint8_t) m_type);
+            // The data.
+            start.Write (m_data.Begin (), m_data.End ());
+        }
 
-void PacketHeader::Print(std::ostream &os) const {
-	os << "data=" << m_data;
-}
+        uint32_t
+        PIMSMHeader::Deserialize(Buffer::Iterator start) {
+            Buffer::Iterator i = start;
+            uint8_t tmp;
+            tmp = start.ReadU8();
+            NS_ASSERT(tmp == 0xfe);
+            tmp = start.ReadU8();
+            NS_ASSERT(tmp == 0xef);
+            m_data = start.ReadNtohU32();
+            uint32_t dist = i.GetDistanceFrom(start);
+            NS_ASSERT(dist == GetSerializedSize());
+            return dist; // the number of bytes consumed.
+        }
 
-void PacketHeader::SetData (uint32_t data){
+        void PIMSMHeader::Print(std::ostream &os) const {
+            switch (m_type) {
+                case PIMSMTYPE_PIMHELLO: {
+                    os << "RREQ";
+                    break;
+                }
+                default:
+                    os << "UNKNOWN_TYPE";
+            }
+        }
 
-	m_data = data;
+        bool
+        PIMSMHeader::operator==(PIMSMHeader const &o) const {
+            return (m_type == o.m_type && m_valid == o.m_valid);
+        }
 
-}
+        std::ostream &
+        operator<<(std::ostream &os, PIMSMHeader const &h) {
+            h.Print(os);
+            return os;
+        }
 
-
-	// ---------------- PIMSM Message -------------------------------
-
-	MessageHeader::MessageHeader ()
-			: m_messageType (MessageHeader::MessageType (0))
-	{
-	}
-
-	MessageHeader::~MessageHeader ()
-	{
-	}
-
-	TypeId
-	MessageHeader::GetTypeId (void)
-	{
-		static TypeId tid = TypeId ("ns3::olsr::MessageHeader")
-				.SetParent<Header> ()
-				.SetGroupName ("Pimsm")
-				.AddConstructor<MessageHeader> ()
-		;
-		return tid;
-	}
-	TypeId
-	MessageHeader::GetInstanceTypeId (void) const
-	{
-		return GetTypeId ();
-	}
-
-	uint32_t
-	MessageHeader::GetSerializedSize (void) const
-	{
-		uint32_t size = OLSR_MSG_HEADER_SIZE;
-		switch (m_messageType)
-		{
-			case HELLO_MESSAGE:
-				//size += m_message.mid.GetSerializedSize ();
-				break;
-			case Register_MESSAGE:
-				//NS_LOG_DEBUG ("Hello Message Size: " << size << " + " << m_message.hello.GetSerializedSize ());
-				size += m_message.hello.GetSerializedSize ();
-				break;
-			case Register_Stop_MESSAGE:
-				//size += m_message.tc.GetSerializedSize ();
-				break;
-			case Join_Prune_MESSAGE:
-				//size += m_message.hna.GetSerializedSize ();
-				break;
-			default:
-				NS_ASSERT (false);
-		}
-		return size;
-	}
-
-	void
-	MessageHeader::Print (std::ostream &os) const
-	{
-		/// \todo
-	}
-
-	void
-	MessageHeader::Serialize (Buffer::Iterator start) const
-	{
-		Buffer::Iterator i = start;
-		i.WriteU8 (m_messageType);
-		i.WriteU8 (m_vTime);
-		i.WriteHtonU16 (GetSerializedSize ());
-		i.WriteHtonU32 (m_originatorAddress.Get ());
-		i.WriteU8 (m_timeToLive);
-		i.WriteU8 (m_hopCount);
-		i.WriteHtonU16 (m_messageSequenceNumber);
-
-		switch (m_messageType)
-		{
-			case HELLO_MESSAGE:
-				//m_message.mid.Serialize (i);
-				break;
-			case Register_MESSAGE:
-				//m_message.hello.Serialize (i);
-				break;
-			case Register_Stop_MESSAGE:
-				//m_message.tc.Serialize (i);
-				break;
-			case Join_Prune_MESSAGE:
-				//m_message.hna.Serialize (i);
-				break;
-			default:
-				NS_ASSERT (false);
-		}
-
-	}
-
-	uint32_t
-	MessageHeader::Deserialize (Buffer::Iterator start)
-	{
-		uint32_t size;
-		Buffer::Iterator i = start;
-		m_messageType  = (MessageType) i.ReadU8 ();
-		NS_ASSERT (m_messageType >= HELLO_MESSAGE && m_messageType <= Join_Prune_MESSAGE);
-		m_vTime  = i.ReadU8 ();
-		m_messageSize  = i.ReadNtohU16 ();
-		m_originatorAddress = Ipv4Address (i.ReadNtohU32 ());
-		m_timeToLive  = i.ReadU8 ();
-		m_hopCount  = i.ReadU8 ();
-		m_messageSequenceNumber = i.ReadNtohU16 ();
-		size = OLSR_MSG_HEADER_SIZE;
-		switch (m_messageType)
-		{
-			case HELLO_MESSAGE:
-				//size += m_message.mid.Deserialize (i, m_messageSize - OLSR_MSG_HEADER_SIZE);
-				break;
-			case Register_MESSAGE:
-				//size += m_message.hello.Deserialize (i, m_messageSize - OLSR_MSG_HEADER_SIZE);
-				break;
-			case Register_Stop_MESSAGE:
-				//size += m_message.tc.Deserialize (i, m_messageSize - OLSR_MSG_HEADER_SIZE);
-				break;
-			case Join_Prune_MESSAGE:
-				//size += m_message.hna.Deserialize (i, m_messageSize - OLSR_MSG_HEADER_SIZE);
-				break;
-			default:
-				NS_ASSERT (false);
-		}
-		return size;
-	}
+//-----------------------------------------------------------------------------
 
 
-	// ---------------- HEllo PIMSM Message -------------------------------
+        void PIMSMHeader::SetData(uint32_t data) {
 
-	TypeId
-	HelloHeader::GetTypeId (void)
-	{
-		static TypeId tid = TypeId ("ns3::pimsm::HelloHeader")
-				.SetParent<Header> ()
-				.SetGroupName ("Pimsm")
-				.AddConstructor<MessageHeader> ()
-		;
-		return tid;
-	}
+            m_data = data;
 
-	TypeId
-	HelloHeader::GetInstanceTypeId (void) const
-	{
-		return GetTypeId ();
-	}
+        }
 
-	uint32_t
-	HelloHeader::GetSerializedSize (void) const
-	{
-		uint32_t size = OLSR_MSG_HEADER_SIZE;
-		return size;
-	}
 
-	void
-	HelloHeader::Print (std::ostream &os) const
-	{
-		/// \todo
-	}
 
-	void
-	HelloHeader::Serialize (Buffer::Iterator start) const
-	{
+//-----------------------------------------------------------------------------
+// Pimsm Hello
+//-----------------------------------------------------------------------------
 
-	}
+        PimsmHelloHeader::PimsmHelloHeader(uint8_t reserved, uint8_t checksum) :
+                m_reserved(reserved), m_checksum(checksum)
+        {
+        }
 
-	uint32_t
-	HelloHeader::Deserialize (Buffer::Iterator start) {
-		uint32_t size = OLSR_MSG_HEADER_SIZE;
-		return size;
-	}
-}
+        NS_OBJECT_ENSURE_REGISTERED (PimsmHelloHeader);
+
+        TypeId
+        PimsmHelloHeader::GetTypeId() {
+            static TypeId tid = TypeId("ns3::pimsm::PimsmHelloHeader")
+                    .SetParent<Header>()
+                    .SetGroupName("Pimsm")
+                    .AddConstructor<PimsmHelloHeader>();
+            return tid;
+        }
+
+        TypeId
+        PimsmHelloHeader::GetInstanceTypeId () const
+        {
+            return GetTypeId ();
+        }
+
+        uint32_t
+        PimsmHelloHeader::GetSerializedSize () const
+        {
+            return 23;
+        }
+
+        void
+        PimsmHelloHeader::Serialize (Buffer::Iterator i) const
+        {
+
+        }
+
+        uint32_t
+        PimsmHelloHeader::Deserialize (Buffer::Iterator start)
+        {
+            Buffer::Iterator i = start;
+
+            m_reserved = i.ReadU8 ();
+            m_checksum = i.ReadU8 ();
+
+            return GetSerializedSize ();
+
+        }
+
+        void
+        PimsmHelloHeader::Print (std::ostream &os) const
+        {
+
+        }
+
+        std::ostream &
+        operator<< (std::ostream & os, PimsmHelloHeader const & h)
+        {
+            h.Print (os);
+            return os;
+        }
+
+    }
 }
--- src/pimsm/model/pimsm-header.h
+++ src/pimsm/model/pimsm-header.h
@@ -1,27 +1,12 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-/*
- * Copyright (c) 2007 INESC Porto
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * Author: Youssef BADDI  <baddi.youssef@gmail.com>
- */
+//
+// Created by baddi on 28/02/16.
+//
+#include <list>
 
 #include "ns3/header.h"
 #include "ns3/address-utils.h"
 #include "ns3/packet.h"
-#include "ns3/nstime.h"
+#include "ns3/ipv4-address.h"
 
 #ifndef NS_3_DEV_MULTICAST_MODULE_PIM_SM_PACKET_H
 #define NS_3_DEV_MULTICAST_MODULE_PIM_SM_PACKET_H
@@ -31,21 +16,49 @@
 namespace ns3 {
 namespace pimsm {
 
-	double EmfToSeconds (uint8_t emf);
-	uint8_t SecondsToEmf (double seconds);
+enum MessageType
+{
+  PIMSMTYPE_PIMHELLO  = 0,   //!< PIMSMTYPE_PIMHELLO
+};
 
-// 3.3.  Packet Format
-//
-//    The basic layout of any packet in OLSR is as follows (omitting IP and
-//    UDP headers):
-//    0                   1                   2                   3
-//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |PIM Ver| Type  |   Reserved    |           Checksum            |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-class PacketHeader: public Header {
+/**
+* \ingroup aodv
+* \brief   Route Request (RREQ) Message Format
+* RFC 7761
+  \verbatim
+  0                   1                   2                   3
+  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |PIM Ver| Type  |   Reserved    |           Checksum            |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  \endverbatim
+
+  PIM Ver
+         PIM Version number is 2.
+
+   Type
+         Types for specific PIM messages.  PIM Types are:
+
+   Message Type                          Destination
+   ---------------------------------------------------------------------
+   0 = Hello                             Multicast to ALL-PIM-ROUTERS
+   1 = Register                          Unicast to RP
+   2 = Register-Stop                     Unicast to source of Register
+                                            packet
+   3 = Join/Prune                        Multicast to ALL-PIM-ROUTERS
+   4 = Bootstrap                         Multicast to ALL-PIM-ROUTERS
+   5 = Assert                            Multicast to ALL-PIM-ROUTERS
+   6 = Graft (used in PIM-DM only)       Unicast to RPF'(S)
+   7 = Graft-Ack (used in PIM-DM only)   Unicast to source of Graft
+                                            packet
+   8 = Candidate-RP-Advertisement        Unicast to Domain's BSR
+*/
+
+
+class PIMSMHeader: public Header {
 public:
+	PIMSMHeader (MessageType t = PIMSMTYPE_PIMHELLO);
 	// must be implemented to become a valid new header.
 	static TypeId GetTypeId(void);
 	virtual TypeId GetInstanceTypeId(void) const;
@@ -57,156 +70,262 @@ public:
 	// allow protocol-specific access to the header data.
 	void SetData(uint32_t data);
 	uint32_t GetData(void) const;
+
+	/// Return type
+	MessageType Get() const {
+		return m_type;
+	}
+	/// Check that type if valid
+	bool IsValid() const {
+		return m_valid;
+	}
+	bool operator==(PIMSMHeader const & o) const;
 private:
-	uint32_t m_data;
+	MessageType m_type;
+	bool m_valid;
+	Buffer m_data;
 };
 
-		// 5.1.  Hello Message Format
-//
-//    The proposed format of a Hello message is as follows:
-//
-//    0                   1                   2                   3
-//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |PIM Ver| Type  |   Reserved    |           Checksum            |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |          OptionType           |         OptionLength          |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |                          OptionValue                          |
-//   |                              ...                              |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |                               .                               |
-//   |                               .                               |
-//   |                               .                               |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |          OptionType           |         OptionLength          |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |                          OptionValue                          |
-//   |                              ...                              |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+std::ostream & operator<< (std::ostream & os, PIMSMHeader const & h);
 
-		class HelloHeader : public Header
-		{
-
-			enum OptionType {
-				Holdtime_TYPE =1,
-			};
-
-		private:
-			OptionType o_optionType;
-
-		public:
-			static TypeId GetTypeId(void);
-			virtual TypeId GetInstanceTypeId(void) const;
-			virtual void Print(std::ostream &os) const;
-			virtual uint32_t GetSerializedSize(void) const;
-			virtual void Serialize(Buffer::Iterator start) const;
-			virtual uint32_t Deserialize(Buffer::Iterator start);
-
-		};
-
-//    0                   1                   2                   3
-//    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-//   |PIM Ver| Type  |   Reserved    |           Checksum            |
-//   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-class MessageHeader: public Header {
+/**
+* \ingroup pimsm
+* \brief   Hello Message Format
+  \verbatim
+   0                   1                   2                   3
+   0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |PIM Ver| Type  |   Reserved    |           Checksum            |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |          OptionType           |         OptionLength          |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                          OptionValue                          |
+   |                              ...                              |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                               .                               |
+   |                               .                               |
+   |                               .                               |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |          OptionType           |         OptionLength          |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                          OptionValue                          |
+   |                              ...                              |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  \endverbatim
+*/
+class PimsmHelloHeader : public PIMSMHeader
+{
 public:
+	PimsmHelloHeader(uint8_t reserved = 0, uint8_t checksum = 0);
 
-	enum MessageType {
-		HELLO_MESSAGE = 1,
-		Register_MESSAGE = 2,
-		Register_Stop_MESSAGE = 3,
-		Join_Prune_MESSAGE = 4,
-		Bootstrap_MESSAGE = 5,
-		Assert_MESSAGE = 6,
-		Graft_MESSAGE = 7,
-		Graft_Ack_MESSAGE = 8,
-		Candidate_RP_Advertisement_MESSAGE = 9,
+	// Header serialization/deserialization
+	  static TypeId GetTypeId ();
+	  TypeId GetInstanceTypeId () const;
+	  uint32_t GetSerializedSize () const;
+	  void Serialize (Buffer::Iterator start) const;
+	  uint32_t Deserialize (Buffer::Iterator start);
+	  void Print (std::ostream &os) const;
+
+	  // Fields
+private:
+	  uint8_t        m_reserved;       ///< Not used
+	  uint8_t        m_checksum;       ///< Checksum
+};
+
+std::ostream & operator<< (std::ostream & os, PimsmHelloHeader const &);
+
+/**
+* \ingroup pimsm
+* \brief   Hello Message Format :  OptionType 1: Holdtime
+  \verbatim
+     0                   1                   2                   3
+     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     |          Type = 1             |         Length = 2            |
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     |          Holdtime             |
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  \endverbatim
+*/
+
+class PimsmHelloHeaderHoldTimeHeader : public PimsmHelloHeader
+{
+public:
+	PimsmHelloHeaderHoldTimeHeader();
+
+	// Header serialization/deserialization
+	static TypeId GetTypeId ();
+	TypeId GetInstanceTypeId () const;
+	uint32_t GetSerializedSize () const;
+	void Serialize (Buffer::Iterator start) const;
+	uint32_t Deserialize (Buffer::Iterator start);
+	void Print (std::ostream &os) const;
+
+	// Fields
+private:
+	uint8_t m_type ;
+	uint8_t m_length;
+	uint8_t m_holdtime;
+};
+
+	std::ostream & operator<< (std::ostream & os, PimsmHelloHeaderHoldTimeHeader const &);
+
+/**
+* \ingroup pimsm
+* \brief   Hello Message Format : OptionType 2: LAN Prune Delay
+  \verbatim
+
+      0                   1                   2                   3
+      0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     |          Type = 2             |          Length = 4           |
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+     |T|      Propagation_Delay      |      Override_Interval        |
+     +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+  \endverbatim
+*/
+
+class PimsmHelloHeaderLANPruneDelayHeader : public PimsmHelloHeader
+	{
+	public:
+		PimsmHelloHeaderLANPruneDelayHeader();
+
+		// Header serialization/deserialization
+		static TypeId GetTypeId ();
+		TypeId GetInstanceTypeId () const;
+		uint32_t GetSerializedSize () const;
+		void Serialize (Buffer::Iterator start) const;
+		uint32_t Deserialize (Buffer::Iterator start);
+		void Print (std::ostream &os) const;
+
+		// Fields
+	private:
+		uint8_t m_type ;
+		uint8_t m_length;
+		uint8_t propagation_Delay;
+		uint8_t override_Interval;
 	};
 
-	MessageHeader ();
-	virtual ~MessageHeader ();
+	std::ostream & operator<< (std::ostream & os, PimsmHelloHeaderLANPruneDelayHeader const &);
 
-	void SetMessageType (MessageType messageType)
-	{
-		m_messageType = messageType;
-	}
-	MessageType GetMessageType () const
-	{
-		return m_messageType;
-	}
+/**
+* \ingroup pimsm
+* \brief   Hello Message Format : OptionType 19: DR Priority
+  \verbatim
 
-	void SetVTime (Time time)
-	{
-		m_vTime = SecondsToEmf (time.GetSeconds ());
-	}
-	Time GetVTime () const
-	{
-		return Seconds (EmfToSeconds (m_vTime));
-	}
+	       0                   1                   2                   3
+	       0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+	      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	      |          Type = 19            |          Length = 4           |
+	      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+	      |                         DR Priority                           |
+	      +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
-	void SetOriginatorAddress (Ipv4Address originatorAddress)
-	{
-		m_originatorAddress = originatorAddress;
-	}
-	Ipv4Address GetOriginatorAddress () const
-	{
-		return m_originatorAddress;
-	}
+  \endverbatim
+*/
 
-	void SetTimeToLive (uint8_t timeToLive)
+	class PimsmHelloHeaderDRPriorityHeader : public PimsmHelloHeader
 	{
-		m_timeToLive = timeToLive;
-	}
-	uint8_t GetTimeToLive () const
-	{
-		return m_timeToLive;
-	}
+	public:
+		PimsmHelloHeaderDRPriorityHeader();
 
-	void SetHopCount (uint8_t hopCount)
-	{
-		m_hopCount = hopCount;
-	}
-	uint8_t GetHopCount () const
-	{
-		return m_hopCount;
-	}
+		// Header serialization/deserialization
+		static TypeId GetTypeId ();
+		TypeId GetInstanceTypeId () const;
+		uint32_t GetSerializedSize () const;
+		void Serialize (Buffer::Iterator start) const;
+		uint32_t Deserialize (Buffer::Iterator start);
+		void Print (std::ostream &os) const;
 
-	void SetMessageSequenceNumber (uint16_t messageSequenceNumber)
-	{
-		m_messageSequenceNumber = messageSequenceNumber;
-	}
-	uint16_t GetMessageSequenceNumber () const
-	{
-		return m_messageSequenceNumber;
-	}
-private:
-	MessageType m_messageType;
-	uint8_t m_vTime;
-	Ipv4Address m_originatorAddress;
-	uint8_t m_timeToLive;
-	uint8_t m_hopCount;
-	uint16_t m_messageSequenceNumber;
-	uint16_t m_messageSize;
+		// Fields
+	private:
+		uint8_t m_type ;
+		uint8_t m_length;
+		uint8_t DR_priority;
+	};
 
-public:
-	static TypeId GetTypeId(void);
-	virtual TypeId GetInstanceTypeId(void) const;
-	virtual void Print(std::ostream &os) const;
-	virtual uint32_t GetSerializedSize(void) const;
-	virtual void Serialize(Buffer::Iterator start) const;
-	virtual uint32_t Deserialize(Buffer::Iterator start);
+	std::ostream & operator<< (std::ostream & os, PimsmHelloHeaderDRPriorityHeader const &);
 
-private:
-	struct
-	{
-		HelloHeader hello;
-	} m_message; // union not allowed
-};
+/**
+* \ingroup pimsm
+* \brief   Hello Message Format : OptionType 20: Generation ID
+  \verbatim
+
+		        0                   1                   2                   3
+		        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+		       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+		       |          Type = 20            |          Length = 4           |
+		       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+		       |                       Generation ID                           |
+		       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 
 
+  \endverbatim
+*/
+
+class PimsmHelloHeaderGenerationIDHeader : public PimsmHelloHeader
+	{
+	public:
+	PimsmHelloHeaderGenerationIDHeader();
+
+		// Header serialization/deserialization
+		static TypeId GetTypeId ();
+		TypeId GetInstanceTypeId () const;
+		uint32_t GetSerializedSize () const;
+		void Serialize (Buffer::Iterator start) const;
+		uint32_t Deserialize (Buffer::Iterator start);
+		void Print (std::ostream &os) const;
+
+		// Fields
+	private:
+		uint8_t m_type ;
+		uint8_t m_length;
+		uint16_t genration_ID;
+	};
+
+	std::ostream & operator<< (std::ostream & os, PimsmHelloHeaderGenerationIDHeader const &);
+
+/**
+* \ingroup pimsm
+* \brief   Hello Message Format : OptionType 24: Address List
+  \verbatim
+			        0                   1                   2                   3
+			        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+			       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+			       |          Type = 24            |      Length = <Variable>      |
+			       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+			       |         Secondary Address 1 (Encoded-Unicast format)          |
+			       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+			                                      ...
+			       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+			       |         Secondary Address N (Encoded-Unicast format)          |
+			       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+  \endverbatim
+*/
+
+class PimsmHelloHeaderAddressListHeader : public PimsmHelloHeader
+	{
+	public:
+	PimsmHelloHeaderAddressListHeader();
+
+		// Header serialization/deserialization
+		static TypeId GetTypeId ();
+		TypeId GetInstanceTypeId () const;
+		uint32_t GetSerializedSize () const;
+		void Serialize (Buffer::Iterator start) const;
+		uint32_t Deserialize (Buffer::Iterator start);
+		void Print (std::ostream &os) const;
+
+		// Fields
+	private:
+		uint8_t m_type ;
+		uint8_t m_length;
+	    std::list<Ipv4Address> SecondaryAddress;
+	};
+
+	std::ostream & operator<< (std::ostream & os, PimsmHelloHeaderAddressListHeader const &);
 
 }
 }
